0082bb98eece80e0480987633575094029c7f63b
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2023-02-12 14:37:44 +0100

    change assets from MessagePage component

--- src/components/layout/MessagePage/MessagePage.tsx
+++ src/components/layout/MessagePage/MessagePage.tsx
@@ -4,12 +4,12 @@ import styles from "./MessagePage.module.scss";
 const messageType = {
   pageNotFound: {
     text: "Ups! page not found",
-    img: "https://img.freepik.com/free-vector/tiny-people-examining-operating-system-error-warning-web-page-isolated-flat-illustration_74855-11104.jpg?w=996&t=st=1665684552~exp=1665685152~hmac=ac9bf94fac80e1abda4518b89b42c3cfc6658bcfce715107555da20cd55f4ea7",
+    img: "./assets/404.png",
     alt: "page not found",
   },
   underConstruction: {
     text: "Sorry, page under construction",
-    img: "https://img.freepik.com/premium-vector/construction-website-page-web-page-vector-illustration_123447-3974.jpg?w=826",
+    img: "./assets/UnderConstruction.png",
     alt: "website underconstruction",
   },
   search: {
