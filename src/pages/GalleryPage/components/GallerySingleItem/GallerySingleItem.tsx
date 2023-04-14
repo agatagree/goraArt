1c09cec4ee7447597beef9e164f0cd724236d01b
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2023-04-14 20:02:31 +0200

    add changing img feature in gallery

--- src/pages/GalleryPage/components/GallerySingleItem/GallerySingleItem.tsx
+++ src/pages/GalleryPage/components/GallerySingleItem/GallerySingleItem.tsx
@@ -1,27 +1,23 @@
-import { forwardRef } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Btn } from "components/common";
 import { ArtDescription } from "components/layout";
 import { GalleryType } from "utils/Types";
 import styles from "./GallerySingleItem.module.scss";
 
-type GallerySingleItemProps = {
-  card: GalleryType;
-};
-
 export const GallerySingleItem = ({ card }: { card: GalleryType }) => {
+  const initialImage = card.img.cover;
+  const [image, setImage] = useState(initialImage);
   return (
     <Btn
       as={Link}
       to={`/gallery/${card.id}`}
       className={styles.art}
       key={card.id}
+      onMouseEnter={() => setImage(card.img.wiz)}
+      onMouseLeave={() => setImage(initialImage)}
     >
-      <img
-        className={styles.artImg}
-        src={card.img.cover}
-        alt={card.title}
-      />
+      <img className={styles.artImg} src={image} alt={card.title} />
       <ArtDescription
         title={card.title}
         code={card.code}
@@ -33,31 +29,3 @@ export const GallerySingleItem = ({ card }: { card: GalleryType }) => {
     </Btn>
   );
 };
-
-// type GallerySingleItemProps = {
-//   card: GalleryType;
-// };
-
-// export const GallerySingleItem = forwardRef<HTMLImageElement, GallerySingleItemProps>(
-//   ({ card, }, ref) => {
-//     return (
-//       <Btn
-//         as={Link}
-//         to={`/gallery/${card.id}`}
-//         className={styles.art}
-//         key={card.id}
-
-//       >
-//         <img className={styles.artImg} src={card.img.cover} alt={card.title} ref={ref}/>
-//         <ArtDescription
-//           title={card.title}
-//           code={card.code}
-//           year={card.year}
-//           technique={card.technique}
-//           width={card.dimensions.width}
-//           height={card.dimensions.height}
-//         />
-//       </Btn>
-//     );
-//   }
-// );
