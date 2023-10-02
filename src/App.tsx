f106f9d8e814ab21d67b8db84b960b0f0a5c7f79
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2023-10-02 14:36:34 +0200

    Change routing and keep gallery data on gallery routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import "react-loading-skeleton/dist/skeleton.css";
 import {
-  Route,
   RouterProvider,
   createBrowserRouter,
-  createRoutesFromElements,
 } from "react-router-dom";
 import { MessagePage } from "components/layout";
 import { RootLayout } from "components/layout";
 import "./i18n";
-import { GalleryPage, HomePage, SingleArtPage } from "pages";
+import { GalleryListPage, GalleryPage, HomePage, SingleArtPage } from "pages";
 import "styles/global.scss";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<HomePage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/gallery/:id" element={<SingleArtPage />} />
-      <Route path="*" element={<MessagePage message={"pageNotFound"} />} />
-    </Route>
-  )
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <MessagePage message={"pageNotFound"} />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/gallery",
+        element: <GalleryPage />,
+        children: [
+          {
+            path: "/gallery",
+            element: <GalleryListPage />,
+          },
+          {
+            path: "/gallery/:id",
+            element: <SingleArtPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 export const App = () => {
   return <RouterProvider router={router} />;
