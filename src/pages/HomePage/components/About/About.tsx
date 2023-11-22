15c67db1b04ca5ab2dded61233351a2612c3a9ac
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2023-11-22 19:53:02 +0100

    Layout change tablet, mobile

--- src/pages/HomePage/components/About/About.tsx
+++ src/pages/HomePage/components/About/About.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
-import { HalfRowLayout } from "components/layout";
+import { TextVariant } from "components/common";
+import { HalfRowLayout, HalfRowLayoutVariant } from "components/layout";
+import styles from "./About.module.scss";
 
 export const About = () => {
   const { t } = useTranslation();
@@ -7,15 +9,17 @@ export const About = () => {
   return (
     <HalfRowLayout
       title={t("home-page.about.title")}
+      titleVariant={TextVariant.basicHeader}
       description={
-        <>
+        <div className={styles.description}>
           {t("home-page.about.description-1")} <br />
           <br /> {t("home-page.about.description-2")} <br />
           <br /> {t("home-page.about.description-3")}{" "}
-        </>
+        </div>
       }
       imgPath="/assets/aboutImg.png"
       artTitle={t("common.img-title")}
+      variant={HalfRowLayoutVariant.reverse}
     />
   );
 };
--- src/pages/HomePage/components/Contact/Contact.tsx
+++ src/pages/HomePage/components/Contact/Contact.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
-import { Text, TextVariant } from "components/common";
+import { ImInstagram, ImFacebook2 } from "react-icons/im";
+import { Btn, Text, TextVariant } from "components/common";
 import { SectionLayout } from "components/layout";
 import styles from "./Contact.module.scss";
 
@@ -10,18 +11,38 @@ export const Contact = () => {
     <SectionLayout className={styles.layout}>
       <div className={styles.wrapper}>
         <div className={styles.title}>
-          <Text variant={TextVariant.mainHeader}>
+          <Text variant={TextVariant.basicHeader}>
             {t("home-page.contact.title")}
           </Text>
         </div>
         <div className={styles.subtitle}>
-          <Text variant={TextVariant.subHeader}>
+          <Text variant={TextVariant.basicHeader}>
             {t("home-page.contact.subtitle")}
           </Text>
         </div>
         <div className={styles.contactInfo}>
           <Text>{t("home-page.contact.e-mail")}</Text>
           <Text>{t("home-page.contact.telephone")}</Text>
+        </div>
+        <div className={styles.icons}>
+          <Btn
+            as={"a"}
+            href="https://www.facebook.com/wojciech.gora/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.iconBtn}
+          >
+            <ImFacebook2 className={styles.icon} />
+          </Btn>
+          <Btn
+            as={"a"}
+            href="https://www.instagram.com/_wojciech_gora_/?fbclid=IwAR3ZjSl3pDKGP8OQgJiLWBWHyf75Zv5Cbu3LZM6QUDxfVRBVANA6BwDBYNc/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.iconBtn}
+          >
+            <ImInstagram className={styles.icon} />
+          </Btn>
         </div>
       </div>
     </SectionLayout>
