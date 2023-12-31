7c886f2e293f3661e1973128dd81b6b4e4391327
Agata Gryszkiewicz <gryszkiewicz.agata@gmail.com>
2023-12-31 13:38:45 +0100

    Move helmet provider to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HelmetProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </HelmetProvider>
 );
